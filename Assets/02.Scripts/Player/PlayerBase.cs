ed5a6c5e5830bfaf4c37b394fe028ea0bee63caf
a01064946264-ui <a01064946264@gmail.com>
2026-02-11 16:52:27 +0900

    [feat] Add PlayerItem
    플레이어 아이템 슬롯을 위한 Dictionary 추가

--- Assets/02.Scripts/Player/PlayerBase.cs
+++ Assets/02.Scripts/Player/PlayerBase.cs
@@ -1,10 +1,16 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerBase : MonoBehaviour, IDamageable
 {
     [SerializeField] private float _hp = 100;
+    [SerializeField] private float _attackPower = 10;
+    [SerializeField] private float _defensePower = 5;
+    [SerializeField] private float _level = 1;
+    [SerializeField] private float _currentExp;
+    [SerializeField] private float _pickupRange = 2;
 
     [SerializeField] private float _moveSpeed = 5;
     [SerializeField] private float _attackInterval = 0.5f;      // 자동공격 주기
@@ -12,17 +18,22 @@ public class PlayerBase : MonoBehaviour, IDamageable
 
 
     private Vector2 _moveDir;
+    private Vector2 _attackDir;
     private SpriteRenderer _mySprite;
     private bool _isInvincible;                                 // 피격 무적상태 여부
     private float _timer;
+    private Dictionary<ITEM, PlayerItem> _myItems;
 
     public float Hp => Mathf.Max(_hp, 0);
+    //public float FinalDamage => _attackPower;                   // 최종 데미지 수치
     public bool IsInvincible => _isInvincible;
+
 
 
     private void Awake()
     {
         _mySprite = GetComponent<SpriteRenderer>();
+        _myItems = new Dictionary<ITEM, PlayerItem>();
     }
 
     private void Update()
@@ -39,11 +50,18 @@ public class PlayerBase : MonoBehaviour, IDamageable
     }
 
 
+
     // 이동 기능
     public void OnMove(InputAction.CallbackContext ctx)
     {
         _moveDir = ctx.ReadValue<Vector2>().normalized;
+
+        if(_moveDir.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+        else if(_moveDir.x > 0)
+            transform.localScale = new Vector3(1, 1, 1);
     }
+
 
     // 공격 기능
     public void OnAttack()
@@ -52,15 +70,21 @@ public class PlayerBase : MonoBehaviour, IDamageable
             return;
 
         // 마우스 위치의 벡터값
-        Vector2 mousePos = Mouse.current.position.ReadValue();
-        Vector2 attackDir = Camera.main.ScreenToWorldPoint(mousePos) - transform.position;
+        SetAttackDir();
 
-        Vector2 spawnPos = (Vector2)transform.position + attackDir.normalized;
+        Vector2 spawnPos = (Vector2)transform.position + _attackDir.normalized;
         var obj = Instantiate(_bulletPrefab, spawnPos, Quaternion.identity);
 
          if (obj.TryGetComponent<IBullet>(out var bullet))
-            bullet.SetDirection(attackDir.normalized);
+            bullet.SetDirection(_attackDir.normalized);
     }
+    // 공격 방향을 결정하는 메서드
+    private void SetAttackDir()
+    {
+        Vector2 mousePos = Mouse.current.position.ReadValue();
+        _attackDir = Camera.main.ScreenToWorldPoint(mousePos) - transform.position;
+    }
+
 
     // 피격시, 발동되는 메서드
     public void TakeDamage(float damage)
@@ -73,7 +97,6 @@ public class PlayerBase : MonoBehaviour, IDamageable
 
         StartCoroutine(StartHitInvincibility());
     }
-
     // 피격 시, 무적 + 피격이펙트(깜빡임)
     private IEnumerator StartHitInvincibility()
     {
@@ -82,7 +105,7 @@ public class PlayerBase : MonoBehaviour, IDamageable
         // 깜빡이는 메서드
         var wait = new WaitForSeconds(0.2f);
         Color original = _mySprite.color;
-        for(int i = 0; i < 5; i++)
+        for(int i = 0; i < 3; i++)
         {
             _mySprite.color = new Color(0, 0, 0);
             yield return wait;
@@ -92,5 +115,10 @@ public class PlayerBase : MonoBehaviour, IDamageable
         }
 
         _isInvincible = false;
+    }
+
+    private enum ITEM
+    {
+        Hat, Cloak, Weapon, Shoes, Relic
     }
 }
