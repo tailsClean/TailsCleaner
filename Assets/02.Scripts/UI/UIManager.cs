9489991e07b27cc64e621c8ed026f4be2fff3804
CommonMango <pjs010105@gmail.com>
2026-02-19 12:12:36 +0900

    [feat] Added UIManager
    UIManager 베이스 작성

---
+++ Assets/02.Scripts/UI/UIManager.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using DG.Tweening;
+using TMPro;
+using System;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager instance;
+    public static UIManager Instance { get => instance; private set => instance = value;}
+    
+    private void Awake()
+    {
+        if(instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public Action OnUIInitialized;
+
+    private void Start()
+    {
+       SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    #region  Scene 초기 설정
+    //▼ UI 설정 오브젝트 
+    [SerializeField] private GameObject currentSceneUI;
+    public Transform stageTrans;
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if(currentSceneUI != null)
+        {
+            Destroy(currentSceneUI);
+        }
+        
+        currentSceneUI = OpenSceneUI($"{scene.name}");
+    }
+
+    private GameObject OpenSceneUI(string sceneName)
+    {
+       GameObject sceneUI = null;
+       GameObject prefab = Resources.Load<GameObject>($"Prefabs/UI/{sceneName}UI");
+
+       if(prefab != null)
+       {
+           sceneUI = Instantiate(prefab, transform);
+       }
+       return sceneUI;
+    }
+
+    #endregion
+
+
+    public void GoToTower()
+    {
+        SceneManager.LoadScene("TowerScene");
+    }
+
+    public void GoToLobby()
+    {
+        SceneManager.LoadScene("LobbyScene");
+    }
+
+    public void GoToStage()
+    {
+        SceneManager.LoadScene("StageScene");
+    }
+}
