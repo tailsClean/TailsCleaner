2f201ec0198ad6a3ce883a16c85725304d1280e1
a01064946264-ui <a01064946264@gmail.com>
2026-02-24 12:56:50 +0900

    [feat] Add ISkillable
    스킬 관련 필요 정보를 전달할 수 있는 인터페이스 추가

---
+++ Assets/02.Scripts/Player/ISkillable.cs
@@ -0,0 +1,14 @@
+﻿
+
+public interface ISkillable
+{
+    public int AttackDamage { get; }
+    public int DefensePower { get; }
+    public int MoveSpeed { get; }
+    public int CriticalChance { get; }
+    public int CriticalDamageMultiplier { get; }    // 치명타 피해 계수
+    public int EvasionChance { get; }               // 회피율
+    public float PickupRange { get; }               // 경험치 획득 범위
+    public float ExperienceGainRate { get; }        // 경헝치 획득량
+}
+
