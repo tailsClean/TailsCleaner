bbbd100c416923a10fc2d49100ac97e1a27ab61d
CommonMango <pjs010105@gmail.com>
2026-03-05 11:35:04 +0900

    Reapply "Merge remote-tracking branch 'origin/refactor/generic-object-pool' into TailsCleaner-0.0.1"
    This reverts commit 487cd0f9467abdc1a0036a552d44a2a76d747a23.

--- Assets/02.Scripts/ObjectPooling/TestSpawner.cs
+++ Assets/02.Scripts/ObjectPooling/TestSpawner.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class TestSpawner : MonoBehaviour
 {
     [Header("Pool Settings")]
-    [SerializeField] private GameObject prefab;
+    [SerializeField] private PoolObject prefab;
     [SerializeField] private int defaultSize = 20;
 
     [Header("Spawn Settings")]
@@ -15,48 +15,35 @@ public class TestSpawner : MonoBehaviour
 
     void Start()
     {
-        // Transform 대신 GameObject로 풀 생성
-        ObjectPoolManager.Instance.CreatePool<GameObject>(prefab, defaultSize);
+        // ObjectPoolManager.Instance.CreatePool(prefab, defaultSize);
     }
 
     void Update()
     {
-        // 즉시 스폰
+        // 즉시 스폰 (마우스 좌클릭)
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("<color=cyan>[Test]</color> 마우스 좌클릭 감지!");
             Spawn();
         }
 
-        // 딜레이 스폰
+        // 딜레이 스폰 (스페이스바)
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            // Debug.Log("<color=yellow>[Test]</color> 딜레이 스폰 시작!");
             StartCoroutine(SpawnWithDelay());
         }
-
     }
 
     private void Spawn()
     {
-        // ObjectPoolManager에서 prefab을 풀에서 꺼내기
-        // Transform 타입으로 가져오고, 현재 위치(transform.position)와 회전(Quaternion.identity) 적용
-        var monster = ObjectPoolManager.Instance.Get<Transform>(prefab.transform, transform.position, Quaternion.identity);
+        if (prefab == null) return;
 
-        // 가져온 오브젝트가 null이 아니고, PoolObject 컴포넌트를 가지고 있다면
-        if (monster != null && monster.TryGetComponent<PoolObject>(out var po))
-        {
-            // po.Setup(): PoolObject에 Release 콜백 등록
-            // 나중에 사용이 끝나면 ObjectPoolManager.Release()를 통해 다시 풀에 반납
-            // Release 호출 시 (프리팹, 인스턴스) 두 개 인자를 넣어야함
-            po.Setup(obj => ObjectPoolManager.Instance.Release<Transform>(prefab.transform, obj.transform));
-        }
+        ObjectPoolManager.Instance.Spawn(prefab, transform.position, Quaternion.identity);
     }
 
     // 지정한 spawnDelay 시간만큼 대기한 뒤 Spawn()을 실행
     private IEnumerator SpawnWithDelay()
     {
-        yield return ObjectPoolManager.Instance.GetWait(spawnDelay);
+        yield return new WaitForSeconds(spawnDelay);
         Spawn();
     }
 }
