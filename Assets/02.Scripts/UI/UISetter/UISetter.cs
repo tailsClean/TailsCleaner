3287ed77905b5c8817be6dd118cdb439ea92910d
a01064946264-ui <a01064946264@gmail.com>
2026-03-05 17:33:57 +0900

    [feat] Add InventoryUI
    인벤토리 UI 추가 완료

---
+++ Assets/02.Scripts/UI/UISetter/UISetter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+public class UISetter : MonoBehaviour
+{
+    [SerializeField] private float Horizental;
+    [SerializeField] private float Virtical;
+    [SerializeField] private float X;
+    [SerializeField] private float Y;
+
+    private RectTransform _mine;
+
+    private void OnValidate()
+    {
+        if(_mine == null)
+            _mine = GetComponent<RectTransform>();
+
+        _mine.sizeDelta = new Vector2(Horizental, Virtical);
+
+        float x = Horizental / 2 + X;
+        float y = Virtical / 2 + Y;
+
+        _mine.position = new Vector2(x, 1080 - y);
+    }
+}
+#endif
