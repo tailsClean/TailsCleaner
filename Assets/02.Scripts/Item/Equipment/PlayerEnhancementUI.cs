fdbe562a27acaba5aeb9e72913d12aefdc345e31
a01064946264-ui <a01064946264@gmail.com>
2026-03-11 12:45:20 +0900

    Revert "[chore] Edit Develop"
    This reverts commit 4e6285f85ca3fa8c97d4c62f628c453540afaf2d.

---
+++ Assets/02.Scripts/Item/Equipment/PlayerEnhancementUI.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 여기 추가 수정 필요
+/// </summary>
+public class PlayerEnhancementUI : MonoBehaviour
+{
+    [SerializeField] private GameObject _playerObj;
+
+    private PlayerEnhancementSelector _player;
+    private Dictionary<EQUIP_PARTS, EquipmentBase> _equipments;
+    public Image _weaponImage;
+    public Image _hatImage;
+    public Image _cloakImage;
+    public Image _shoseImage;
+    public Image[] _relicSlot;
+
+    public Sprite baseImg;
+
+    private void Awake()
+    {
+        _player = _playerObj.GetComponent<PlayerEnhancementSelector>();
+        if (_player == null)
+            Debug.LogWarning("UI출력을 위한 플레이어가 제대로 세팅되지 않음");
+    }
+
+    private void OnEnable()
+    {
+        _player.OnSetEquipment += UpdateImage;
+        _player.OnSetRelic += UpdateImage;
+    }
+
+    private void OnDisable()
+    {
+        _player.OnSetEquipment -= UpdateImage;
+    }
+
+    private void Start()
+    {
+        _equipments = _player.MyEquipment;
+
+        foreach (var equip in _equipments)
+            UpdateImage(equip.Key);
+    }
+
+
+    // UI갱신 메서드
+    private void UpdateImage(EQUIP_PARTS equipmentType)
+    {
+        var equipment = _player.MyEquipment[equipmentType];
+
+        switch (equipmentType)
+        {
+            //case EQUIP_PARTS.Weapon:
+            //    _weaponImage.sprite = equipment.SpriteImage;
+            //    break;
+            //case EQUIP_PARTS.Hat:
+            //    _hatImage.sprite = equipment.SpriteImage;
+            //    break;
+            //case EQUIP_PARTS.Shoes:
+            //    _shoseImage.sprite = equipment.SpriteImage;
+            //    break;
+            //case EQUIP_PARTS.Cloak:
+            //    _cloakImage.sprite = equipment.SpriteImage;
+            //    break;
+        }
+    }
+
+    private void UpdateImage()
+    {
+        var relics = _player.MyRelic;
+        for(int i = 0; i < relics.Count; i++)
+        {
+            if(relics[i] == null)
+            {
+                _relicSlot[i].sprite = baseImg;
+                continue;
+            }
+
+            //_relicSlot[i].sprite = relics[i].SpriteImage;
+        }
+    }
+}
