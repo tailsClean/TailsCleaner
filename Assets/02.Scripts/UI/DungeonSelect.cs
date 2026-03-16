05391f319f7019eb5c329fc22829133f2e798ba1
CommonMango <pjs010105@gmail.com>
2026-03-16 10:51:14 +0900

    [feat]Added Base DungeonSelect
    던전 선택 ui 베이스 추가

---
+++ Assets/02.Scripts/UI/DungeonSelect.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+using System.Collections.Generic;
+public class DungeonSelect : MonoBehaviour
+{
+    [SerializeField] Button _nextBtn; //다음 버튼
+    [SerializeField] Button _prvBtn; //이전 버튼 
+    List<GameObject> _towers; //타워 패널들이 들어있는 리스트 
+
+
+
+    
+}
