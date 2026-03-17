5bbffdd77b4ee88aec1215acd18aa7585ca1dd62
CommonMango <pjs010105@gmail.com>
2026-03-17 14:38:58 +0900

    [chore] Added Slider
    skill효과음과 몬스터 효과음 조정 추가

--- Assets/02.Scripts/UI/ExitPanel.cs
+++ Assets/02.Scripts/UI/ExitPanel.cs
@@ -6,6 +6,8 @@ public class ExitPanel : MonoBehaviour
 {
     [SerializeField] private Button _exitButton;
     [SerializeField] private Slider _bGMSlider;
+    [SerializeField] private Slider _skillSFXSlider;
+    [SerializeField] private Slider _monsterSFXSlider;
 
     public void Start()
     {
@@ -17,6 +19,9 @@ public class ExitPanel : MonoBehaviour
         }
 
         _bGMSlider?.onValueChanged.AddListener (value => SoundManager.Instance.SetBGMVolume(value));
+        _skillSFXSlider?.onValueChanged.AddListener(value => SoundManager.Instance.SetSkillSFXVolume(value));
+        _monsterSFXSlider?.onValueChanged.AddListener(value => SoundManager.Instance.SetMonsterSFXVolume(value));
+
 
     }
 }
