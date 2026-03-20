a9009022fd4d634c96fc16d32ef9b305d9632c86
KIS <csdiary1234@gmail.com>
2026-03-20 15:56:00 +0900

    [feat] Add login
    로그인시스템 추가 완료

---
+++ Assets/02.Scripts/UI/Login/LoginSystem.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginSystem : MonoBehaviour
+{
+    [Header("로그인 패널")]
+    [SerializeField] private GameObject _login1;
+    [SerializeField] private GameObject _login2;
+
+    [Header("------------------------------------------------------")]
+
+    [Header("체크박스")]
+    [SerializeField] private CheckBoxSprites _chekBoxSprites;
+    [SerializeField] private List<CheckBox> _checkBoxes;
+
+    private void Awake()
+    {
+        foreach(var checkBox in _checkBoxes)
+        {
+            checkBox.checkBoxSprites = _chekBoxSprites;
+        }
+    }
+
+    private void OnEnable()
+    {
+        _login2?.SetActive(false);
+    }
+
+    private void Start()
+    {
+        SetButton();
+    }
+
+    private void OnDestroy()
+    {
+        foreach (var checkBox in _checkBoxes)
+        {
+            checkBox.button.onClick.RemoveAllListeners();
+        }
+    }
+
+
+    private void SetButton()
+    {
+        foreach(var checkBox in _checkBoxes)
+        {
+            checkBox.button.onClick.AddListener(checkBox.SetImg);
+        }
+    }
+
+
+    [Serializable]
+    private class CheckBoxSprites
+    {
+        public Sprite CheckImg;
+        public Sprite UnDoCheckImg;
+    }
+
+    [Serializable]
+    private class CheckBox
+    {
+        public Button button;
+        public Image image;
+        public bool isChecked = false;
+
+        public CheckBoxSprites checkBoxSprites { get; set; }
+
+
+        public void SetImg() 
+        {
+            if (checkBoxSprites == null)
+                return;
+
+            if (!isChecked)
+                image.sprite = checkBoxSprites.CheckImg;
+            else
+                image.sprite = checkBoxSprites.UnDoCheckImg;
+        }
+    }
+
+
+    private void OnValidate()
+    {
+        if (_checkBoxes == null)
+            return;
+
+        foreach(var checkBox in _checkBoxes)
+        {
+            if(checkBox.button != null)
+                checkBox.image = checkBox.button.GetComponent<Image>();
+        }
+    }
+}
---
+++ Assets/02.Scripts/UI/Login/TextLink.cs
@@ -0,0 +1,21 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TextLink : MonoBehaviour, IPointerClickHandler
+{
+    public TextMeshProUGUI text;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        int linkIndex = TMP_TextUtilities.FindIntersectingLink(text, eventData.position, null);
+
+        if (linkIndex != -1)
+        {
+            TMP_LinkInfo linkInfo = text.textInfo.linkInfo[linkIndex];
+            string url = linkInfo.GetLinkID();
+
+            Application.OpenURL(url);
+        }
+    }
+}
