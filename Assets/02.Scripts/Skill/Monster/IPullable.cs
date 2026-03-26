d89482993fbab0f9d0747a62bd6fd042c47de56e
MelonCorn <rhfoem456@gmail.com>
2026-03-26 15:03:43 +0900

    [Feat] Monster, Projectile Pull
    물폭탄 소용돌이 모으기 기능
    몬스터, 몬스터 투사체 적용

--- Assets/02.Scripts/Skill/Monster/IPullable.cs
+++ Assets/02.Scripts/Skill/Monster/IPullable.cs
@@ -5,5 +5,5 @@ public interface IPullable
     /// <summary>
     /// 목표 지점으로 끌어당기기
     /// </summary>
-    void Pull(Vector2 targetPosition, float force);
+    void Pull(Vector2 targetPosition);
 }
