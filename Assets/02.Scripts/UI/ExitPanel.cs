40286f736d9c683fb8552c3e16146876af9b0ae0
CommonMango <pjs010105@gmail.com>
2026-03-26 18:17:42 +0900

    [chore] edit UI
    UI  폰트 변경
    일부 UI 위치 변경

--- Assets/02.Scripts/UI/ExitPanel.cs
+++ Assets/02.Scripts/UI/ExitPanel.cs
@@ -5,17 +5,20 @@ using UnityEngine.UIElements.Experimental;
 public class ExitPanel : MonoBehaviour
 {
     [SerializeField] private Button _exitButton;
+    [SerializeField] private Button _settingExitBtn;
+    [SerializeField] private Button _saveSetting;
     [SerializeField] private Slider _bGMSlider;
     [SerializeField] private Slider _skillSFXSlider;
-    [SerializeField] private Slider _monsterSFXSlider;
+   
 
     public void Start()
     {
         _exitButton.onClick.AddListener(UIManager.Instance.GoToLobby);
+        _settingExitBtn.onClick.AddListener(UIManager.Instance.ChangeStateSettingPanel);
         _bGMSlider?.onValueChanged.AddListener (value => SoundManager.Instance.SetBGMVolume(value));
         _skillSFXSlider?.onValueChanged.AddListener(value => SoundManager.Instance.SetSkillSFXVolume(value));
-        _monsterSFXSlider?.onValueChanged.AddListener(value => SoundManager.Instance.SetMonsterSFXVolume(value));
 
     }
+    
 }
 
