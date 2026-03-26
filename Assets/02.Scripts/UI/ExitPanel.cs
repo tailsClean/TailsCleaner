33c7bdd9e7ed32a92c8665dafad9b184a153d01b
CommonMango <pjs010105@gmail.com>
2026-03-26 20:00:26 +0900

    [chore] add cancelquit
    나가기버튼  리소스 추가

--- Assets/02.Scripts/UI/ExitPanel.cs
+++ Assets/02.Scripts/UI/ExitPanel.cs
@@ -14,7 +14,6 @@ public class ExitPanel : MonoBehaviour
     public void Start()
     {
         _exitButton.onClick.AddListener(OnClickExit);
-        _exitButton.onClick.AddListener(UIManager.Instance.GoToLobby);
         _settingExitBtn.onClick.AddListener(UIManager.Instance.ChangeStateSettingPanel);
         _bGMSlider?.onValueChanged.AddListener (value => SoundManager.Instance.SetBGMVolume(value));
         _skillSFXSlider?.onValueChanged.AddListener(value => SoundManager.Instance.SetSkillSFXVolume(value));
