722f2a1fe127136e23f063af796f31ae3c49b5f8
KIS <csdiary1234@gmail.com>
2026-04-01 17:46:19 +0900

    [fix] Add Reward DropRate
    보상 드랍확률 기능 추가

--- Assets/02.Scripts/Reward/RewardCreator.cs
+++ Assets/02.Scripts/Reward/RewardCreator.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using Unity.Mathematics;
-using UnityEngine;
 
 public class RewardCreator
 {
@@ -32,15 +30,34 @@ public class RewardCreator
         var dataBundle = RewardDB.GetRewardTable(StageGroupID);
         foreach(var rewardData in dataBundle.datas)
         {
+            // 획득할 수 없다면 획득x(첫 클리어시에만 습득하는 보상 필터링)
             if (rewardData.isNonReward)
+                continue;
+
+            // 드랍확률에 들어가지 않으면 획득x
+            if(!CheckDropable(rewardData))
                 continue;
 
             SetCurrentReward(rewardData.data);
 
+            // 첫 클리어 보상일 경우 isNonReward = true
             RewardTable data = rewardData.data;
             if(data.reward_category == REWARD_CATEGORY.First)
                 rewardData.isNonReward = true;
         }
+    }
+
+    // 드랍확률에 들어가지 않으면 획득x
+    private bool CheckDropable(RewardDataBundle.RewardData reward)
+    {
+        int dropPercent = (int)reward.data.drop_rate * 100;
+        int random = UnityEngine.Random.Range(1, 101);
+
+        if (random <= dropPercent)
+            return true;
+        
+        else
+            return false;
     }
 
     // 현재 보상에 값(아이템의 정보, 획득 수량)을 세팅
@@ -63,6 +80,7 @@ public class RewardDTO
         Items = new List<ItemInstance>();
     }
 
+    // 보상의 종류에 따라 보상을 분류해서 추가함
     public void SetDTO(RewardTable rewardData, int amount)
     {
         switch (rewardData.reward_type)
